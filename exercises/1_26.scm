79b12b480d40d9930a9eaf6f75169dbe9034e4c4
Damiano Presa <presadamiano@gmail.com>
2021-02-01 23:19:40 +0100

    new solution

--- exercises/1_26.scm
+++ exercises/1_26.scm
@@ -1,1 +1,7 @@
-(define ()))
+(define (accumulate combiner null-value func a next b)
+  (if (> a b)
+    null-value
+    (combiner (func a) (accumulate combiner (combiner null-value a) func (next a) next b))))
+
+(define (accumulate combiner null-value func a next b)
+    (define (accumulate-iter combiner null-value func a next b result )))
