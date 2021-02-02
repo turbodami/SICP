caf9b80e1940a0966ba31c2e7f9fc3e4a080c991
Damiano Presa <presadamiano@gmail.com>
2021-02-02 23:50:44 +0100

    2.13

---
+++ exercises/2_13.scm
@@ -0,0 +1,12 @@
+a = Ca ± δa
+b = Cb ± δb
+ab = CaCb ± (Caδb + Cbδa + δaδb)
+ab = CaCb ± (Caδb + Cbδa) + δaδb
+ab = Cacb + δaδb ± (Caδb + Cbδa)
+Now δa and δb are small, so ignoring δaδb
+Cab = CaCb
+δab = Caδb + Cbδa
+Since tolerances, T are given as percentages
+Tab = δab/Cab
+Tab = (Caδb + Cbδa) / CaCb
+Tab = δb/Cb + δa/Ca
