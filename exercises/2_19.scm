2569ecb67e17d5e2021408a7719d5f5ab6eaa2cb
Damiano Presa <presadamiano@gmail.com>
2021-02-15 23:14:38 +0100

    2.19

---
+++ exercises/2_19.scm
@@ -0,0 +1,16 @@
+(define us-coins (list 100 50 25 10 5 1))
+(define uk-coins (list 200 100 50 20 10 5 2 1))
+ 
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0)
+             (no-more? coin-values)) 0)
+        (else (+ (cc amount
+                     (except-first-denomination coin-values))
+                 (cc (- amount
+                        (first-denomination coin-values))
+                     coin-values)))))
+ 
+(define (first-denomination coins) (car coins))
+(define (except-first-denomination coins) (cdr coins))
+(define (no-more? coins) (null? coins))
