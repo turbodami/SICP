0372429cb2b6711fc037b34db940df14ec37db2b
Damiano Presa <presadamiano@gmail.com>
2021-03-14 21:51:57 +0100

    2.33

---
+++ exercises/2_33.scm
@@ -0,0 +1,14 @@
+(define (map p sequence)
+  (accumulate (lambda (x y) 
+                (cons (p x) y)) 
+              nil 
+              sequence))
+ 
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+ 
+(define (length sequence)
+  (accumulate (lambda (a b) 
+                (+ b 1)) 
+              0 
+              sequence))
